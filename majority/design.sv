b6654a201441a2585a5ca624e735ded89546dd7a
Mac Baker <bakerjm24450@gmail.com>
2021-09-15 12:33:13 +0000

    Update for Fall 2021 to work with newer symbiflow

--- majority/design.sv
+++ majority/design.sv
@@ -1,6 +1,7 @@
 // 3-input majority gate
 // Output is 1 if majority of inputs are 1
-module majority(input logic a,
+//module majority(input logic a,
+module minority(input logic a,
                 input logic b,
                 input logic c,
                 output logic y);
